b628c31d68510d269e93558893127659b016ef67
Joe Bert Sembrero <jbrt.dev@gmail.com>
2024-12-09 22:47:25 +0800

    init: AdminLoginUseCase implementation

---
+++ src/application/use-cases/auth/AdminLoginUseCase.ts
@@ -0,0 +1,49 @@
+import { IUserRepository } from "src/application/ports/repositories/IUserRepository";
+import { IAdminLoginUseCase, AdminLoginUseCaseResponse } from "./interfaces/IAdminLoginUseCase";
+import { IPasswordHasher } from "src/application/ports/services/IPasswordHasher";
+import { ITokenService } from "src/application/ports/services/ITokenService";
+
+export class AdminLoginUseCase implements IAdminLoginUseCase {
+    constructor(
+        private readonly repository: IUserRepository,
+        private readonly password_service: IPasswordHasher,
+        private readonly token_service: ITokenService
+    ) { }
+
+    public async execute(username: string, password: string): Promise<AdminLoginUseCaseResponse> {
+        // Gets the user from datasource
+        const user = await this.repository.findOneBy([
+            {
+                field: 'username',
+                value: username
+            }
+        ]);
+
+        // If user doesn't exist, return error
+        if (!user) return {
+            errors: [{ field: 'username', message: 'User not found' }],
+            result: { user: null, token: null }
+        }
+
+        // Check if provided password is correct
+        const is_password_matched = await this.password_service.compare(password, user.password);
+
+        // If password is incorrect, return error
+        if (!is_password_matched) return {
+            errors: [{ field: 'password', message: 'Password is incorrect' }],
+            result: { user: null, token: null }
+        }
+
+        // Removes password from user object
+        const { password: _omit_password, ...user_obj } = user;
+
+        // Generates auth token
+        const auth_token = await this.token_service.generate(user_obj);
+
+        // Return user and token
+        return {
+            errors: [],
+            result: { user: user_obj, token: auth_token }
+        }
+    }
+}

a7d53d09e228a6592e6e6f06fcddd1625db3c8c1
Joe Bert Sembrero <jbrt.dev@gmail.com>
2024-12-11 23:44:55 +0800

    added: validation when requesting to create a user

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -3,11 +3,36 @@ import { createUserController } from "src/infrastructure/api/users/index";
 import { CreateUserControllerInput } from 'src/interfaces/controllers/ICreateUserController';
 const router = express.Router();
 
-router.get('/create', async (req, res) => {
+router.post('/create', async (req, res) => {
+    if (!req.body.username) {
+        res.send({ 
+            data: null, 
+            errors: [{ message: "username is required", field: "username" }]
+        });
+        return;
+    }
+
+    if (!req.body.password) {
+        res.send({ 
+            data: null, 
+            errors: [{ message: "password is required", field: "password" }]
+        });
+        return;
+    }
+
+    if (!req.body.type) {
+        res.send({ 
+            data: null, 
+            errors: [{ message: "type is required", field: "type" }]
+        });
+        return;
+    }
+
+    const { type, username, password } = req.body
     const user_input_sample: CreateUserControllerInput = {
-        Type: 'SUPERVISOR',
-        Password: 'password',
-        Username: 'emjay_supervisor'
+        Type: type,
+        Password: password,
+        Username: username
     }
     const response = await createUserController.handle(user_input_sample)
     res.send(response);

f8b2b16c6b92457389934854a680ddd374794e01
Joe Bert Sembrero <jbrt.dev@gmail.com>
2024-12-11 23:46:33 +0800

    added: validation when requesting to login

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -3,8 +3,20 @@ import { adminLoginController } from "src/infrastructure/controllers/auth/index"
 
 const router = express.Router();
 
-router.get('/login', async (req, res) => {
-    const response = await adminLoginController.handle({ username: 'emjay', password: 'password' });
+router.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+
+    if (!username) {
+        res.send({ data: null, errors: [{ message: "username is required", field: "username" }] });
+        return;
+    }
+
+    if (!password) {
+        res.send({ data: null, errors: [{ message: "password is required", field: "password" }] });
+        return;
+    }
+
+    const response = await adminLoginController.handle({ username, password });
     res.send(response);
 });
 

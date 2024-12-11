cd882ac9de78131e8d03548b0f607851f71d58c9
Joe Bert Sembrero <jbrt.dev@gmail.com>
2024-12-11 23:17:25 +0800

    changed: implementation in users api

--- src/infrastructure/api/users/index.ts
+++ src/infrastructure/api/users/index.ts
@@ -1,19 +1,20 @@
 import { CreateUserUseCase } from "src/application/use-cases/users/CreateUserUseCase";
 import { CreateUserController } from "src/infrastructure/controllers/users/CreateUserController";
 import { MockUserRepository } from "src/infrastructure/repositories/mocks/MockUserRepository";
-import { UserRepository } from "src/infrastructure/repositories/UserRepository";
+import { MongoUserRepository } from "src/infrastructure/repositories/mongodb/MongoUserRepository";
 import { MockPasswordHasher } from "src/infrastructure/services/PasswordHasher";
 
 // Repositories
-const user_repository = process.env.IS_PRODUCTION 
-    ? new UserRepository()
+const mongo_user_repository = new MongoUserRepository()
+const user_repository = process.env.IS_PRODUCTION === 'YES'
+    ? new MongoUserRepository()
     : new MockUserRepository();
 
 // Services
 const mock_password_service = new MockPasswordHasher();
 
 // Use Cases
-const createUserUseCase = new CreateUserUseCase(user_repository, mock_password_service);
+const createUserUseCase = new CreateUserUseCase(mongo_user_repository, mock_password_service);
 
 // Controllers
 export const createUserController = new CreateUserController(createUserUseCase);

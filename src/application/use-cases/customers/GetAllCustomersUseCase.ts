34ba3f0bb3d9d02534b189ab2cde392f0ac7d023
Joe Bert Sembrero <jbrt.dev@gmail.com>
2024-12-14 09:33:20 +0800

    init: GetAllCustomersUseCase implementation

---
+++ src/application/use-cases/customers/GetAllCustomersUseCase.ts
@@ -0,0 +1,17 @@
+import { ICustomerRepository } from "src/application/ports/repositories/ICustomerRepository";
+import { IGetAllCustomersUseCase } from "./interfaces/IGetAllCustomers";
+import { CustomerObject } from "./interfaces/common";
+import { custom } from "joi";
+
+export class GetAllCustomersUseCase implements IGetAllCustomersUseCase {
+
+    constructor(private readonly customer_repository: ICustomerRepository) { }
+
+    async execute(): Promise<Omit<CustomerObject, 'password'>[]> {
+        const customers = await this.customer_repository.retrieveAll();
+
+        return customers.map(({ password: ommited_pass, ...customer }) => ({
+            ...customer
+        }));
+    }
+}

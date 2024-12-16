48cb356e1b4750687c23d76eb518ec53e4ebe418
Joe Bert Sembrero <jbrt.dev@gmail.com>
2024-12-16 19:43:43 +0800

    init: GetCustomerServicesCount transctions

--- src/infrastructure/controllers/customers/index.ts
+++ src/infrastructure/controllers/customers/index.ts
@@ -1,12 +1,18 @@
 import { MongoCustomerRepository } from "src/infrastructure/repositories/mongodb/CustomerRepository";
 import { GetAllCustomersController } from "./GetAllCustomersController";
 import { GetAllCustomersUseCase } from "src/application/use-cases/customers/GetAllCustomersUseCase";
+import { CustomerServicesRepository } from "src/infrastructure/repositories/mongodb/CustomerServicesRepository";
+import { GetCustomerServicesUseCase } from "src/application/use-cases/customers/GetServicesCountUseCase";
+import { CustomerServicesController } from "./CustomerServicesController";
 
 // Repositories
 const customer_repository = new MongoCustomerRepository();
+const customer_services_repository = new CustomerServicesRepository();
 
 // Use Cases
 const getAllCustomersUseCase = new GetAllCustomersUseCase(customer_repository);
+const getCustomerServicesUseCase = new GetCustomerServicesUseCase(customer_services_repository);
 
 // Controllers
-export const getAllCustomersController = new GetAllCustomersController(getAllCustomersUseCase)
+export const getAllCustomersController = new GetAllCustomersController(getAllCustomersUseCase);
+export const getCustomerServicesController = new CustomerServicesController(getCustomerServicesUseCase);

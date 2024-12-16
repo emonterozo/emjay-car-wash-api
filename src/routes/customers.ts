e208abff9aeedb906a9752262f7e4b4b6f98877d
Joe Bert Sembrero <jbrt.dev@gmail.com>
2024-12-16 19:44:03 +0800

    added: route for customer services count

--- src/routes/customers.ts
+++ src/routes/customers.ts
@@ -1,10 +1,18 @@
 import express from 'express';
-import { getAllCustomersController } from 'src/infrastructure/controllers/customers';
+import { getAllCustomersController, getCustomerServicesController } from 'src/infrastructure/controllers/customers';
 const router = express.Router();
 
 // Get All entries route
 router.get('/', async (req, res) => {
     const customer_reponse = await getAllCustomersController.handle();
+
+    res.send(customer_reponse);
+});
+
+// Get customer services by id
+router.get('/:customer_id/services', async (req, res) => {
+    const { customer_id } = req.params;
+    const customer_reponse = await getCustomerServicesController.handle(customer_id);
 
     res.send(customer_reponse);
 });

a6df867858dba495aafceda6fb10e82db126dd43
Joe Bert Sembrero <jbrt.dev@gmail.com>
2024-12-18 00:48:53 +0800

    added: GetOneCustomerUseCase as deps of CustomerServicesController

--- src/infrastructure/controllers/customers/index.ts
+++ src/infrastructure/controllers/customers/index.ts
@@ -4,15 +4,20 @@ import { GetAllCustomersUseCase } from "src/application/use-cases/customers/GetA
 import { CustomerServicesRepository } from "src/infrastructure/repositories/mongodb/CustomerServicesRepository";
 import { GetCustomerServicesUseCase } from "src/application/use-cases/customers/GetServicesCountUseCase";
 import { CustomerServicesController } from "./CustomerServicesController";
+import { GetOneCustomerUseCase } from "src/application/use-cases/customers/GetOneCustomerUseCase";
 
 // Repositories
 const customer_repository = new MongoCustomerRepository();
 const customer_services_repository = new CustomerServicesRepository();
 
 // Use Cases
+const getOneCustomerUseCase = new GetOneCustomerUseCase(customer_repository)
 const getAllCustomersUseCase = new GetAllCustomersUseCase(customer_repository);
 const getCustomerServicesUseCase = new GetCustomerServicesUseCase(customer_services_repository);
 
 // Controllers
 export const getAllCustomersController = new GetAllCustomersController(getAllCustomersUseCase);
-export const getCustomerServicesController = new CustomerServicesController(getCustomerServicesUseCase);
+export const getCustomerServicesController = new CustomerServicesController(
+    getCustomerServicesUseCase,
+    getOneCustomerUseCase
+);

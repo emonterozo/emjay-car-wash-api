942dcac94a02730eac677d9980ce049a31e641f4
Joe Bert Sembrero <jbrt.dev@gmail.com>
2024-12-18 00:45:53 +0800

    added: customer id validation in CustomerServicesController class

--- src/infrastructure/controllers/customers/CustomerServicesController.ts
+++ src/infrastructure/controllers/customers/CustomerServicesController.ts
@@ -1,11 +1,13 @@
 import { IGetAllCustomersUseCase } from "src/application/use-cases/customers/interfaces/IGetAllCustomers";
+import { IGetOneCustomerUseCase } from "src/application/use-cases/customers/interfaces/IGetOneCustomerUseCase";
 import { IGetServicesCountUseCase } from "src/application/use-cases/customers/interfaces/IGetServicesCountUseCase";
 import { CustomerServCountResponse, ICustomerServicesController } from "src/interfaces/controllers/customers/IGetCustomerServicesController";
 
 export class CustomerServicesController implements ICustomerServicesController {
 
     constructor(
-        private readonly _get_customer_services: IGetServicesCountUseCase
+        private readonly _get_customer_services: IGetServicesCountUseCase,
+        private readonly _get_customer: IGetOneCustomerUseCase
     ) { }
 
     public async handle(id: string): Promise<CustomerServCountResponse> {
@@ -14,7 +16,15 @@ export class CustomerServicesController implements ICustomerServicesController {
             data: { customer_services: null }
         }
 
+        // Validates if customer exists
+        const { errors: is_exist_err, result: is_exist_res } = await this._get_customer.execute({ id });
+        const  { customer } =  is_exist_res;
+        if (!customer) return {
+            data: { customer_services: null },
+            errors: is_exist_err
+        }
 
+        // Get customer services
         const { result, errors } = await this._get_customer_services.execute(id);
         const { customer_services } = result;
 

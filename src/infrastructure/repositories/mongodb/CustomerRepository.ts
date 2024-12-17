a8b71281e93a484a184cdb2970a552f0253b4116
Joe Bert Sembrero <jbrt.dev@gmail.com>
2024-12-18 00:46:45 +0800

    added: implementation of findOneBy in CustomerRepository class

--- src/infrastructure/repositories/mongodb/CustomerRepository.ts
+++ src/infrastructure/repositories/mongodb/CustomerRepository.ts
@@ -1,6 +1,6 @@
 import { Collection, MongoClient, ObjectId, ServerApiVersion } from "mongodb";
 import { ICustomerRepository } from "src/application/ports/repositories/ICustomerRepository";
-import { CustomerObject } from "src/application/use-cases/customers/interfaces/common";
+import { CustomerFilterInput, CustomerObject } from "src/application/use-cases/customers/interfaces/common";
 
 export class MongoCustomerRepository implements ICustomerRepository {
 
@@ -22,5 +22,28 @@ export class MongoCustomerRepository implements ICustomerRepository {
             ...customer
         }));
         return result
+    }
+
+    public async findOneBy(conditions: CustomerFilterInput): Promise<CustomerObject | null> {
+        await this.client.connect();
+        const db = this.client.db(process.env.MONGO_DATASOURCE);
+        const collection: Collection<Omit<CustomerObject, 'id'> & { _id: ObjectId }> = db.collection(process.env.MONGO_CUSTOMERS_COLLECTION!);
+
+        const { id, ...polished_filters }: CustomerFilterInput & { _id?: ObjectId } = conditions;
+        if (id) polished_filters['_id'] = new ObjectId(id);
+
+        const result = await collection.findOne({ 
+            ...polished_filters
+        });
+
+        if (!result)
+            return null;
+
+        const { _id, ...customer } = result;
+
+        return {
+            id: _id.toString(),
+            ...customer
+        }
     }
 }

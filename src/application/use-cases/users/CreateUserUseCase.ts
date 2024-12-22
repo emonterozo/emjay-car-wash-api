425210a03179844c7e72feff74ca98d50cba3b08
jbrtsmbrr <jbrt.dev@gmail.com>
2024-12-22 12:46:13 +0800

    refactor: added validation in CreateUserUseCase class

--- src/application/use-cases/users/CreateUserUseCase.ts
+++ src/application/use-cases/users/CreateUserUseCase.ts
@@ -1,31 +1,45 @@
 import { IUserRepository } from "src/application/ports/repositories/IUserRepository";
-import { CreateUserInput, UserObject, UserType } from "./interfaces.ts/common";
+import { CreateUserInput, UserType } from "./interfaces.ts/common";
 import { ICreateUserUseCase, ICreateUseUseCaseResult } from "./interfaces.ts/ICreateUserUseCase";
 import { UserEntity } from "../../../domain/entities/UserEntity";
 import { IPasswordHasher } from "src/application/ports/services/IPasswordHasher";
+import { ErrorMessage } from "src/application/ports/common";
 
 export class CreateUserUseCase implements ICreateUserUseCase {
     constructor(
         private readonly repository: IUserRepository,
         private readonly password_service: IPasswordHasher
-    ) {}
+    ) { }
 
     public async execute(input: CreateUserInput): ICreateUseUseCaseResult {
         const userEntity = UserEntity.create(input);
-        const isValid = userEntity.validateEmail();
+        const validation_errors: ErrorMessage[] = [];
 
-        if (!isValid) return {
-            errors: [{ message: "Invalid email", field: "email" }],
-            result: null
-        }
+        // Validates user input fields
+        const usernameErrMsg = userEntity.validateUsername();
+        const passwordErrMsg = userEntity.validatePassword();
 
+        if (usernameErrMsg)
+            validation_errors.push({ field: 'username', message: usernameErrMsg })
+
+        if (passwordErrMsg)
+            validation_errors.push({ field: 'password', message: passwordErrMsg })
+
+        // If one of input values is invalid return immediately
+        if (validation_errors.length)
+            return { result: null, errors: validation_errors }
+
+        // Hashes the validated password
         const hashed_password = await this.password_service.hash(userEntity.getPassword());
+
+        // Saves the validated inputs
         const inserted_id = await this.repository.create({
             type: userEntity.getUserType() as UserType,
             password: hashed_password,
             username: userEntity.getUsername()
         });
 
+        // Retrieves newly inserted user
         const inserted_user = await this.repository.retrieve(inserted_id);
 
         return {

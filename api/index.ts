653fc3a078eb6a9427d968d5f3d39d3e8ba5aa94
Eric Monterozo <ric.monterozo@gmail.com>
2024-12-27 00:39:20 +0800

    chore: trial setup

--- api/index.ts
+++
@@ -1,5 +0,0 @@
-require('dotenv').config();
-import app from '../app';
-
-
-app.listen(process.env.PORT, () => console.log(`now listening on port ${process.env.PORT}`));
--- server.ts
+++ server.ts
@@ -1,8 +1,4 @@
 require('dotenv').config();
 import app from './app.js';
 
-app.use((req, res, next) => {
-  console.log(`Received request: ${req.method} ${req.url}`);
-  next();
-});
 app.listen(process.env.PORT, () => console.log(`now listening on port ${process.env.PORT}`));

4afde1f150fa204cd34cc3f5f2ec6e5cf8648279
Eric Monterozo <ric.monterozo@gmail.com>
2024-12-26 23:29:03 +0800

    chore: add logs

--- server.ts
+++ server.ts
@@ -1,4 +1,8 @@
 require('dotenv').config();
 import app from './app.js';
 
+app.use((req, res, next) => {
+  console.log(`Received request: ${req.method} ${req.url}`);
+  next();
+});
 app.listen(process.env.PORT, () => console.log(`now listening on port ${process.env.PORT}`));

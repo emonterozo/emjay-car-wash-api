0ef420eab2c81712ac2c79eb574ed42c65699741
Eric Monterozo <ric.monterozo@gmail.com>
2024-12-27 01:29:21 +0800

    chore: trial without path alias

--- app.ts
+++ app.ts
@@ -19,10 +19,10 @@ app.use(helmet());
 app.use(cors());
 
 app.use('/home', homeRouter);
-app.use('/users', userRouter);
-app.use('/admin', AdminRouter);
-app.use('/customers', CustomersRouter);
-app.use('/services', ServicesRouter);
+// app.use('/users', userRouter);
+// app.use('/admin', AdminRouter);
+// app.use('/customers', CustomersRouter);
+// app.use('/services', ServicesRouter);
 
 app.get('/', (req, res) => {
   res.sendStatus(200);

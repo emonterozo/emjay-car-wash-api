2574aa2a85352077dabdd454b99e5d9582ade3bf
jbrtsmbrr <jbrt.dev@gmail.com>
2024-12-27 13:53:32 +0800

    refactor: added pagination params in ICustomerRepository interface

--- src/application/ports/repositories/ICustomerRepository.ts
+++ src/application/ports/repositories/ICustomerRepository.ts
@@ -1,9 +1,10 @@
+import { IGetAllCustomerParams } from 'src/application/use-cases/customers/interfaces/IGetAllCustomers';
 import {
   CustomerFilterInput,
   CustomerObject,
 } from '../../../application/use-cases/customers/interfaces/common';
 
 export interface ICustomerRepository {
-  retrieveAll(): Promise<CustomerObject[]>;
+  retrieveAll(params?: IGetAllCustomerParams): Promise<CustomerObject[]>;
   findOneBy(conditions: CustomerFilterInput): Promise<CustomerObject | null>;
 }

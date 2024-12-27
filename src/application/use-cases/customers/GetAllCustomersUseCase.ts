37324054e74b70646be1f2aed7bff2ae9aff071e
jbrtsmbrr <jbrt.dev@gmail.com>
2024-12-27 13:54:54 +0800

    refactor: handle pagination params in GetAllCustomersUseCase class

--- src/application/use-cases/customers/GetAllCustomersUseCase.ts
+++ src/application/use-cases/customers/GetAllCustomersUseCase.ts
@@ -1,13 +1,16 @@
 import { ICustomerRepository } from '../../../application/ports/repositories/ICustomerRepository';
-import { IGetAllCustomersUseCase } from './interfaces/IGetAllCustomers';
+import { IGetAllCustomerParams, IGetAllCustomersUseCase } from './interfaces/IGetAllCustomers';
 import { CustomerObject } from './interfaces/common';
-import { custom } from 'joi';
 
 export class GetAllCustomersUseCase implements IGetAllCustomersUseCase {
   constructor(private readonly customer_repository: ICustomerRepository) {}
 
-  async execute(): Promise<Omit<CustomerObject, 'password'>[]> {
-    const customers = await this.customer_repository.retrieveAll();
+  async execute(params?: IGetAllCustomerParams): Promise<Omit<CustomerObject, 'password'>[]> {
+    const customers = await this.customer_repository.retrieveAll({
+      limit: params?.limit ?? 0,
+      offset: params?.offset ?? 0,
+      order_by: params?.order_by ?? { field: 'first_name', direction: 'asc' }
+    });
 
     return customers.map(({ password: ommited_pass, ...customer }) => ({
       ...customer,

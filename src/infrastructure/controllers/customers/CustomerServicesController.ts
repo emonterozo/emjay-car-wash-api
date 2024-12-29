a68bdbc88d08391c38e101a1ea5923f559828c6d
jbrtsmbrr <jbrt.dev@gmail.com>
2024-12-30 00:45:17 +0800

    refactor: handled unauthenticated requests in CustomerServicesController class

--- src/infrastructure/controllers/customers/CustomerServicesController.ts
+++ src/infrastructure/controllers/customers/CustomerServicesController.ts
@@ -1,3 +1,4 @@
+import { ITokenService } from 'src/application/ports/services/ITokenService';
 import { IGetAllCustomersUseCase } from '../../../application/use-cases/customers/interfaces/IGetAllCustomers';
 import { IGetOneCustomerUseCase } from '../../../application/use-cases/customers/interfaces/IGetOneCustomerUseCase';
 import { IGetServicesCountUseCase } from '../../../application/use-cases/customers/interfaces/IGetServicesCountUseCase';
@@ -10,9 +11,19 @@ export class CustomerServicesController implements ICustomerServicesController {
   constructor(
     private readonly _get_customer_services: IGetServicesCountUseCase,
     private readonly _get_customer: IGetOneCustomerUseCase,
-  ) {}
+    private readonly _token_service: ITokenService
+  ) { }
 
-  public async handle(id: string): Promise<CustomerServCountResponse> {
+  public async handle(token: string, id: string): Promise<CustomerServCountResponse> {
+
+    const is_valid_token = await this._token_service.verify(token);
+
+    if (!is_valid_token) return {
+      data: { customer_services: null },
+      errors: [{ field: 'unknown', message: 'UNAUTHENTICATED_REQUEST' }]
+    };
+
+
     if (typeof id !== 'string' || id.length === 0)
       return {
         errors: [{ field: 'id', message: 'Invalid ID' }],

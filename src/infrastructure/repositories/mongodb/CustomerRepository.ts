63842b3fd0b1ffff06602223fee7d6fe8015b502
jbrtsmbrr <jbrt.dev@gmail.com>
2024-12-29 20:23:08 +0800

    refactor: added customer collection type in CustomerRepository class

--- src/infrastructure/repositories/mongodb/CustomerRepository.ts
+++ src/infrastructure/repositories/mongodb/CustomerRepository.ts
@@ -7,13 +7,43 @@ import {
 import { MongoDB } from './MongoDB';
 import { IGetAllCustomerParams } from 'src/application/use-cases/customers/interfaces/IGetAllCustomers';
 
+interface IServiceTransaction {
+  _id: ObjectId;
+  service_id: ObjectId;
+  service: string;
+  price: number;
+  date: string;
+}
+
+interface IServiceCount {
+  size: "sm" | "md" | "lg" | "xl" | "xxl";
+  count: number;
+}
+
+interface ICustomerCollection {
+  _id: ObjectId;
+  first_name: string;
+  last_name: string;
+  birth_date: string;
+  contact_number: string;
+  password: string;
+  province: string;
+  city: string;
+  barangay: string;
+  address: string;
+  registered_on: string;
+  moto_wash_service_count: IServiceCount[];
+  car_wash_service_count: IServiceCount[];
+  recent_transactions: IServiceTransaction[];
+}
+
 export class MongoCustomerRepository implements ICustomerRepository {
   private readonly client = MongoDB.instance();
 
   public async retrieveAll(params?: IGetAllCustomerParams): Promise<CustomerObject[]> {
     await this.client.connect();
     const db = this.client.db(process.env.MONGO_DATASOURCE);
-    const collection: Collection<Omit<CustomerObject, 'id'> & { _id: ObjectId }> = db.collection(
+    const collection: Collection<ICustomerCollection> = db.collection(
       process.env.MONGO_CUSTOMERS_COLLECTION!,
     );
     

3afa7ed7cccc4aea6de78ff27f021d4b6f759f89
jbrtsmbrr <jbrt.dev@gmail.com>
2024-12-29 14:26:08 +0800

    refactor: apply new total field in GetAllCustomersController class

--- src/infrastructure/controllers/customers/GetAllCustomersController.ts
+++ src/infrastructure/controllers/customers/GetAllCustomersController.ts
@@ -1,3 +1,4 @@
+import { IGetCustomersCountUseCase } from 'src/application/use-cases/customers/interfaces/IGetCustomersCountUseCase';
 import { IGetAllCustomerParams, IGetAllCustomersUseCase } from '../../../application/use-cases/customers/interfaces/IGetAllCustomers';
 import {
   GetAllCustomersControllerOutput,
@@ -5,14 +6,19 @@ import {
 } from '../../../interfaces/controllers/customers/IGetAllCustomersController';
 
 export class GetAllCustomersController implements IGetAllCustomersController {
-  constructor(private readonly usecase: IGetAllCustomersUseCase) {}
+  constructor(
+    private readonly usecase: IGetAllCustomersUseCase,
+    private readonly get_customers_total: IGetCustomersCountUseCase
+  ) {}
 
   public async handle(params?: IGetAllCustomerParams): Promise<GetAllCustomersControllerOutput> {
     const customers = await this.usecase.execute(params);
+    const count_result = await this.get_customers_total.execute();
 
     return {
       data: {
         customers,
+        total: count_result.result.total
       },
       errors: [],
     };

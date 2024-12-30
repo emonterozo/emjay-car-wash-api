59eb9d1fd3e416ab1cffd556c7c17e69659d36ee
jbrtsmbrr <jbrt.dev@gmail.com>
2024-12-30 20:09:10 +0800

    fixed: removed unused classes

--- src/infrastructure/controllers/customers/index.ts
+++ src/infrastructure/controllers/customers/index.ts
@@ -1,8 +1,6 @@
 import { MongoCustomerRepository } from '../../../infrastructure/repositories/mongodb/CustomerRepository';
 import { GetAllCustomersController } from './GetAllCustomersController';
 import { GetAllCustomersUseCase } from '../../../application/use-cases/customers/GetAllCustomersUseCase';
-import { CustomerServicesRepository } from '../../../infrastructure/repositories/mongodb/CustomerServicesRepository';
-import { GetCustomerServicesUseCase } from '../../../application/use-cases/customers/GetServicesCountUseCase';
 import { CustomerServicesController } from './CustomerServicesController';
 import { GetOneCustomerUseCase } from '../../../application/use-cases/customers/GetOneCustomerUseCase';
 import { GetCustomersCountUseCase } from '../../../application/use-cases/customers/GetCustomersCountUseCase';
@@ -13,18 +11,15 @@ const token_service = new TokenService();
 
 // Repositories
 const customer_repository = new MongoCustomerRepository();
-const customer_services_repository = new CustomerServicesRepository();
 
 // Use Cases
 const getOneCustomerUseCase = new GetOneCustomerUseCase(customer_repository);
 const getAllCustomersUseCase = new GetAllCustomersUseCase(customer_repository);
-const getCustomerServicesUseCase = new GetCustomerServicesUseCase(customer_services_repository);
 const getCustomersCountUseCase = new GetCustomersCountUseCase(customer_repository)
 
 // Controllers
 export const getAllCustomersController = new GetAllCustomersController(getAllCustomersUseCase, getCustomersCountUseCase, token_service);
 export const getCustomerServicesController = new CustomerServicesController(
-  // getCustomerServicesUseCase,
   getOneCustomerUseCase,
   token_service
 );

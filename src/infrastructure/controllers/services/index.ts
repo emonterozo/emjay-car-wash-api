3be95d4bf818e9c2ac03e87580905855467a1a07
jbrtsmbrr <jbrt.dev@gmail.com>
2024-12-30 11:05:06 +0800

    refactor: added token service as deps of GetAllServicesController class

--- src/infrastructure/controllers/services/index.ts
+++ src/infrastructure/controllers/services/index.ts
@@ -1,6 +1,10 @@
+import { TokenService } from 'src/infrastructure/services/TokenService';
 import { GetAllServicesUseCase } from '../../../application/use-cases/services/GetAllServicesUseCase';
 import { ServiceRepository } from '../../../infrastructure/repositories/mongodb/ServiceRepository';
 import { GetAllServicesController } from './GetAllServicesController';
+
+// Services
+const token_service = new TokenService();
 
 // Repositories
 const service_repository = new ServiceRepository();
@@ -9,4 +13,4 @@ const service_repository = new ServiceRepository();
 const get_all_services_use_case = new GetAllServicesUseCase(service_repository);
 
 // Controllers
-export const get_all_services_controller = new GetAllServicesController(get_all_services_use_case);
+export const get_all_services_controller = new GetAllServicesController(get_all_services_use_case, token_service);

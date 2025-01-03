72fdf64d695b6559a3da58b6335f9018f958ba37
Joe Bert Sembrero <jbrt.dev@gmail.com>
2025-01-03 21:58:52 +0800

    refactor: added 'and' and 'or' conditions in IGetAllTransactionsController

--- src/interfaces/controllers/transactions/IGetAllTransactionsController.ts
+++ src/interfaces/controllers/transactions/IGetAllTransactionsController.ts
@@ -1,9 +1,9 @@
 import { TransactionObject } from "src/application/use-cases/transactions/interfaces/common"
 import { ControllerResponse } from "../common"
-import { Range } from "src/application/ports/repositories/common"
+import { Condition, Range } from "src/application/ports/repositories/common"
 
 export type IGetAllTransactionsControllerResult = ControllerResponse<{ transactions: TransactionObject[] } | null>
 
 export interface IGetAllTransactionsController {
-  handle(token: string, params: { range: Range }): Promise<IGetAllTransactionsControllerResult>
+  handle(token: string, params: { range: Range; and_conditions: Condition[]; or_conditions: Condition[] }): Promise<IGetAllTransactionsControllerResult>
 }

00b7baf4ae531c2447671e5dc44e75aef944bfff
Joe Bert Sembrero <jbrt.dev@gmail.com>
2025-01-05 00:01:38 +0800

    refactor: apply IGetAllTransactionsParams in IGetAllTransactionsController interface

--- src/interfaces/controllers/transactions/IGetAllTransactionsController.ts
+++ src/interfaces/controllers/transactions/IGetAllTransactionsController.ts
@@ -1,9 +1,10 @@
 import { TransactionObject } from "src/application/use-cases/transactions/interfaces/common"
 import { ControllerResponse } from "../common"
 import { Condition, Range } from "src/application/ports/repositories/common"
+import { IGetAllTransactionsParams } from "src/application/use-cases/transactions/interfaces/IGetAllTransactionUseCase"
 
 export type IGetAllTransactionsControllerResult = ControllerResponse<{ transactions: TransactionObject[] } | null>
 
 export interface IGetAllTransactionsController {
-  handle(token: string, params: { range: Range; and_conditions: Condition[]; or_conditions: Condition[] }): Promise<IGetAllTransactionsControllerResult>
+  handle(token: string, params: IGetAllTransactionsParams): Promise<IGetAllTransactionsControllerResult>
 }

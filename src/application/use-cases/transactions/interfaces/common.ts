4623451168cb3c0e31f867bcacd3219aec9e93d5
Joe Bert Sembrero <jbrt.dev@gmail.com>
2025-01-08 00:20:45 +0800

    refactor: updated TransactionService ommon interfaces

--- src/application/use-cases/transactions/interfaces/common.ts
+++ src/application/use-cases/transactions/interfaces/common.ts
@@ -1,21 +1,28 @@
 import { CustomerId } from "../../customers/interfaces/common";
 import { ServiceId } from "../../services/interfaces/common";
 
-export interface TransactionDetails {
-  vehicle_type: string;
-  vehicle_size: string;
-  model: string;
-  price: number;
+interface TransactionService {
+  id: ServiceId;
   deduction: number;
   company_earnings: number;
   employee_share: number;
-  check_ing: string;
-  completed_on: string;
-  is_free: boolean;
-  claimed_by: string;
-  service_id: string;
-  customer_id: string;
   assigned_employee_id: string[];
+  start_date?: Date;
+  end_date?: Date;
+  status: string;
+  is_free: boolean;
+}
+
+export interface TransactionDetails {
+  customer_id?: string;
+  vehicle_type: string;
+  vehicle_size: string;
+  model: string;
+  plate_number: string;
+  contact_number?: string;
+  check_in: Date;
+  // status: string; // TODO: This should be computed depending on the services status
+  services: TransactionService[]
 }
 
 export interface TransactionObject extends TransactionDetails {

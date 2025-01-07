b3f915572c26bb9117a70f1382df2334882de981
Joe Bert Sembrero <jbrt.dev@gmail.com>
2025-01-07 23:17:31 +0800

    refactor: updated entry point of transactions api

--- src/infrastructure/controllers/transactions/index.ts
+++ src/infrastructure/controllers/transactions/index.ts
@@ -1,15 +1,19 @@
 // Services
 
 
+import { CreateTransactionUseCase } from "../../../application/use-cases/transactions/CreateTransactionUseCase";
 import { GetAllTransactionsUseCase } from "../../../application/use-cases/transactions/GetAllTransactionsUseCase";
 import { TransactionRepository } from "../../../infrastructure/repositories/mongodb/TransactionRepository";
 import { GetAllTransactionsController } from "./GetAllTransactionsController";
+import { CreateTransactionController } from "./CreateTransactionController";
 
 // Repository
 const transaction_repository = new TransactionRepository();
 
 // Use Cases
 const getAllTransactions = new GetAllTransactionsUseCase(transaction_repository);
+const createTranasction = new CreateTransactionUseCase(transaction_repository);
 
 // Controllers
 export const getAllTransactionsController = new GetAllTransactionsController(getAllTransactions);
+export const createTransactionController = new CreateTransactionController(createTranasction);

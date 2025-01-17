2de906303fa8ff5b35ba80739cb7df16d77d9cf2
Joe Bert Sembrero <jbrt.dev@gmail.com>
2025-01-17 12:10:51 +0800

    refactor: added optional(temporary) status and success field in ControllerResponse interface

--- src/interfaces/controllers/common.ts
+++ src/interfaces/controllers/common.ts
@@ -3,4 +3,6 @@ import { ErrorMessage } from '../../application/ports/common';
 export interface ControllerResponse<T> {
   data: T;
   errors: ErrorMessage[];
+  status?: number;
+  success?: boolean;
 }

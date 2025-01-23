d8e5c5914a33e0a9d8bd060256126ca06c11d241
Eric Monterozo <ric.monterozo@gmail.com>
2025-01-23 15:50:08 +0800

    refactor: uri of getCustomerWashCountById controller

--- src/routes/customersRoutes.ts
+++ src/routes/customersRoutes.ts
@@ -10,6 +10,10 @@ const router = express.Router();
 
 router.get('/', authenticateToken as any, getAllCustomers as any);
 router.get('/:customer_id', authenticateToken as any, getCustomerById as any);
-router.get('/scan/:customer_id', authenticateToken as any, getCustomerWashCountById as any);
+router.get(
+  '/:customer_id/wash-service-count',
+  authenticateToken as any,
+  getCustomerWashCountById as any,
+);
 
 export default router;

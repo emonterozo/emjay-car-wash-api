14fb4daa3c6c3078ff22a42846ea0a5fdade035d
Eric Monterozo <ric.monterozo@gmail.com>
2025-01-23 15:58:01 +0800

    refactor: ongoing route uri to follow REST API standard

--- src/routes/ongoingRoutes.ts
+++ src/routes/ongoingRoutes.ts
@@ -12,18 +12,18 @@ const router = express.Router();
 
 router.get('/transactions', authenticateToken as any, getTransactions as any);
 router.get(
-  '/transaction/:transaction_id/services',
+  '/transactions/:transaction_id/services',
   authenticateToken as any,
   getTransactionServicesById as any,
 );
 router.get(
-  '/transaction/:transaction_id/service/:transaction_service_id',
+  '/transactions/:transaction_id/services/:transaction_service_id',
   authenticateToken as any,
   getTransactionServiceById as any,
 );
 router.post('/transactions', authenticateToken as any, createOngoingTransaction as any);
 router.put(
-  '/transaction/:transaction_id/service',
+  '/transactions/:transaction_id/services',
   authenticateToken as any,
   addTransactionService as any,
 );

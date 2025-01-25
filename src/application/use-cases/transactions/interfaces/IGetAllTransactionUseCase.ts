ba302a42610a491c5e0416a11b287acccfad4cb2
Joe Bert Sembrero <jbrt.dev@gmail.com>
2025-01-25 22:00:44 +0800

    refactor: rename TransactionService to AvailedService in ICreateTransactionUseCase interface

--- src/application/use-cases/transactions/interfaces/IGetAllTransactionUseCase.ts
+++ src/application/use-cases/transactions/interfaces/IGetAllTransactionUseCase.ts
@@ -1,16 +1,16 @@
 import { Condition, OrderBy, Range } from "src/application/ports/repositories/common";
 import { UseCaseResult } from "../../common";
-import { TransactionObject, TransactionService } from "./common";
+import { TransactionObject, AvailedService } from "./common";
 
 type ConcatKeyWithString<T, S extends string> = `${S}.${keyof T & string}`;
 
 export interface IGetAllTransactionsParams {
   limit?: number;
   offset?: number;
-  order_by?: OrderBy<keyof Omit<TransactionObject, 'services'> | ConcatKeyWithString<TransactionService, 'services'>>;
-  and_conditions?: Condition<keyof Omit<TransactionObject, 'services'> | ConcatKeyWithString<TransactionService, 'services'>>[];
-  or_conditions?: Condition<keyof Omit<TransactionObject, 'services'> | ConcatKeyWithString<TransactionService, 'services'>>[];
-  not?: Condition<keyof Omit<TransactionObject, 'services'> | ConcatKeyWithString<TransactionService, 'services'>>[];
+  order_by?: OrderBy<keyof Omit<TransactionObject, 'services'> | ConcatKeyWithString<AvailedService, 'services'>>;
+  and_conditions?: Condition<keyof Omit<TransactionObject, 'services'> | ConcatKeyWithString<AvailedService, 'services'>>[];
+  or_conditions?: Condition<keyof Omit<TransactionObject, 'services'> | ConcatKeyWithString<AvailedService, 'services'>>[];
+  not?: Condition<keyof Omit<TransactionObject, 'services'> | ConcatKeyWithString<AvailedService, 'services'>>[];
   range?: Range;
 }
 

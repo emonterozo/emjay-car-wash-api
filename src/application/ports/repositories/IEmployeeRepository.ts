35b52dc45d339fe521eed37c2cd3ed00cccfcf79
Joe Bert Sembrero <jbrt.dev@gmail.com>
2025-01-28 00:10:21 +0800

    refactor: added count method in employee repository

--- src/application/ports/repositories/IEmployeeRepository.ts
+++ src/application/ports/repositories/IEmployeeRepository.ts
@@ -4,4 +4,5 @@ import { FindAllOptions } from "./common";
 
 export interface IEmployeeRepository {
     findAll(options?: FindAllOptions<EmployeeObject>): Promise<EmployeeObject[]>;
+    count(options?: FindAllOptions<EmployeeObject>): Promise<number>;
 }

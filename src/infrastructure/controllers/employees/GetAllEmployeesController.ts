2167419b72f0cd0d8536c1d09733849bda04c905
Joe Bert Sembrero <jbrt.dev@gmail.com>
2025-01-28 00:09:54 +0800

    refactor: added support for token validation

--- src/infrastructure/controllers/employees/GetAllEmployeesController.ts
+++ src/infrastructure/controllers/employees/GetAllEmployeesController.ts
@@ -1,16 +1,37 @@
-import { ICountEmployeesUseCase } from "src/application/use-cases/employees/interfaces/ICountEmployeesUseCase";
-import { IGetAllEmployeesUseCase } from "src/application/use-cases/employees/interfaces/IGetAllEmployeesUseCase";
-import { ControllerResponse } from "src/interfaces/controllers/common";
-import { EmployeeControllerOutput, EmployeeOutput, IGetAllEmployeesController } from "src/interfaces/controllers/employees/IGetAllEmployeesController";
+import { ITokenService } from "../../../application/ports/services/ITokenService";
+import { ICountEmployeesUseCase } from "../../../application/use-cases/employees/interfaces/ICountEmployeesUseCase";
+import { IGetAllEmployeesUseCase } from "../../../application/use-cases/employees/interfaces/IGetAllEmployeesUseCase";
+import { EmployeeControllerOutput, IGetAllEmployeesController } from "../../../interfaces/controllers/employees/IGetAllEmployeesController";
 
 export class GetAllEmployeesController implements IGetAllEmployeesController {
 
     constructor(
         private readonly getEmployeesUseCase: IGetAllEmployeesUseCase,
         private readonly countEmployeesUseCase: ICountEmployeesUseCase,
+        private readonly _token_service: ITokenService
     ) { }
 
     public async handle(token: string): Promise<EmployeeControllerOutput> {
+
+        if (!token) {
+            return {
+                data: null,
+                errors: [{ field: 'Authorization', message: 'Token is missing.' }],
+                status: 401,
+                success: false
+            };
+        }
+
+        const is_valid_token = await this._token_service.verify(token);
+
+        if (!is_valid_token)
+            return {
+                data: null,
+                errors: [{ field: 'Authorization', message: 'Invalid or expired token.' }],
+                status: 403,
+                success: false
+            };
+
         const { errors, result } = await this.getEmployeesUseCase.execute();
         const { errors: count_errors, result: count_result } = await this.countEmployeesUseCase.execute();
 

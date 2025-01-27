14e8fccd5cdd12bb4fd86a09d0c1ad29c2ae9242
Joe Bert Sembrero <jbrt.dev@gmail.com>
2025-01-28 00:09:25 +0800

    refactor: added getter of total employee count

--- src/infrastructure/controllers/employees/GetAllEmployeesController.ts
+++ src/infrastructure/controllers/employees/GetAllEmployeesController.ts
@@ -1,20 +1,25 @@
+import { ICountEmployeesUseCase } from "src/application/use-cases/employees/interfaces/ICountEmployeesUseCase";
 import { IGetAllEmployeesUseCase } from "src/application/use-cases/employees/interfaces/IGetAllEmployeesUseCase";
 import { ControllerResponse } from "src/interfaces/controllers/common";
 import { EmployeeControllerOutput, EmployeeOutput, IGetAllEmployeesController } from "src/interfaces/controllers/employees/IGetAllEmployeesController";
 
 export class GetAllEmployeesController implements IGetAllEmployeesController {
 
-    constructor(private readonly getEmployeesUseCase: IGetAllEmployeesUseCase) { }
+    constructor(
+        private readonly getEmployeesUseCase: IGetAllEmployeesUseCase,
+        private readonly countEmployeesUseCase: ICountEmployeesUseCase,
+    ) { }
 
     public async handle(token: string): Promise<EmployeeControllerOutput> {
         const { errors, result } = await this.getEmployeesUseCase.execute();
+        const { errors: count_errors, result: count_result } = await this.countEmployeesUseCase.execute();
 
-        const no_errors = errors.length === 0;
+        const no_errors = [...errors, ...count_errors].length === 0;
 
         return {
             data: {
                 employees: result.employees,
-                totalCount: 0
+                totalCount: count_result.count
             },
             errors: errors,
             status: no_errors ? 200 : 500,

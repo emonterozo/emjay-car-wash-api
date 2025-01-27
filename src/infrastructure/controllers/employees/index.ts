8542797928e02c56fc8f971e9b0f3b6874d44ff6
Joe Bert Sembrero <jbrt.dev@gmail.com>
2025-01-28 00:12:03 +0800

    refactor: added token service

--- src/infrastructure/controllers/employees/index.ts
+++ src/infrastructure/controllers/employees/index.ts
@@ -1,3 +1,5 @@
+import { TokenService } from "../../../infrastructure/services/TokenService";
+import { CountEmployeesUseCase } from "../../../application/use-cases/employees/CountEmployeesUseCase";
 import { GetAllEmployeeesUseCase } from "../../../application/use-cases/employees/GetAllEmployeesUseCase";
 import { EmployeeRepository } from "../../../infrastructure/repositories/mongodb/EmployeeRepository";
 import { GetAllEmployeesController } from "./GetAllEmployeesController";
@@ -5,8 +7,16 @@ import { GetAllEmployeesController } from "./GetAllEmployeesController";
 // Repositories
 const employee_repository = new EmployeeRepository();
 
+// Services
+const token_service = new TokenService();
+
 // Use Cases
 const getAllEmployees = new GetAllEmployeeesUseCase(employee_repository);
+const countEmployees = new CountEmployeesUseCase(employee_repository);
 
 // Controllers
-export const get_all_employees_controller = new GetAllEmployeesController(getAllEmployees);
+export const get_all_employees_controller = new GetAllEmployeesController(
+    getAllEmployees,
+    countEmployees,
+    token_service
+);

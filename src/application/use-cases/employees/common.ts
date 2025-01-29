fe1362f9256fd949c8b2a7bc09e40390e2c55869
Joe Bert Sembrero <jbrt.dev@gmail.com>
2025-01-29 23:24:15 +0800

    refactor: updated employee common interfaces

--- src/application/use-cases/employees/common.ts
+++ src/application/use-cases/employees/common.ts
@@ -6,8 +6,13 @@ export interface EmployeeDetails {
     gender: "MALE" | "FEMALE";
     employee_title: string;
     employee_status: "ACTIVE" | "TERMINATED";
+    contact_number: string;
+    birth_date: Date;
+    date_started: Date;
 }
 
 export interface EmployeeObject extends EmployeeDetails {
     id: EmployeeId;
 }
+
+export interface EmployeeFilterInput extends Partial<EmployeeObject> {}

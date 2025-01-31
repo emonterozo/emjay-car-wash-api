75e170c2bf4e0309880ea094f72004194fa5abe3
Joe Bert Sembrero <jbrt.dev@gmail.com>
2025-01-31 22:39:16 +0800

    refactor: added 'create' method in IEmployeeRepository interface

--- src/application/ports/repositories/IEmployeeRepository.ts
+++ src/application/ports/repositories/IEmployeeRepository.ts
@@ -1,9 +1,11 @@
-import { EmployeeFilterInput, EmployeeObject } from "src/application/use-cases/employees/common";
-import { FindAllOptions } from "./common";
+import { EmployeeDetails, EmployeeFilterInput, EmployeeObject } from "src/application/use-cases/employees/common";
+import { FindAllOptions, InsertedId } from "./common";
 
+export interface CreateEmployeeParams extends EmployeeDetails {}
 
 export interface IEmployeeRepository {
     findAll(options?: FindAllOptions<EmployeeObject>): Promise<EmployeeObject[]>;
     count(options?: FindAllOptions<EmployeeObject>): Promise<number>;
     findOne(filters?: EmployeeFilterInput): Promise<EmployeeObject | null>;
+    create(params: CreateEmployeeParams): Promise<InsertedId | null>;
 }

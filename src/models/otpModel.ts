bef34ab0846582e79bbe5003bcedd3a5fc31c723
Eric Monterozo <ric.monterozo@gmail.com>
2025-02-28 13:47:48 +0800

    feat: otp model

--- src/models/otpModel.ts
+++ src/models/otpModel.ts
@@ -3,7 +3,7 @@ import mongoose, { Types } from 'mongoose';
 const otpSchema = new mongoose.Schema({
   customer_id: { type: Types.ObjectId, ref: 'Customer' },
   otp: { type: Number, required: true },
-  created_at: { type: Date, default: Date.now, expires: 300 },
+  created_at: { type: Date, default: Date.now, expires: 290 },
 });
 
 const Otp = mongoose.model('Otp', otpSchema);

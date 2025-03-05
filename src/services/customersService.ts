cbeaef0bd70c2c73bda302d615c8b3f9913df9ad
Eric Monterozo <ric.monterozo@gmail.com>
2025-03-05 14:39:05 +0800

    feat: customer wash count

--- src/services/customersService.ts
+++ src/services/customersService.ts
@@ -101,7 +101,7 @@ export const getCustomerWashCountById = async (customer_id: string) => {
       }[] = [];
 
       document.car_wash_service_count.forEach((item) => {
-        if (item.count > 10) {
+        if (item.count >= 10) {
           free_wash.push({
             size: item.size,
             count: item.count,
@@ -111,7 +111,7 @@ export const getCustomerWashCountById = async (customer_id: string) => {
       });
 
       document.moto_wash_service_count.forEach((item) => {
-        if (item.count > 10) {
+        if (item.count >= 10) {
           free_wash.push({
             size: item.size,
             count: item.count,
